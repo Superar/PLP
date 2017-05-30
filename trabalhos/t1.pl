8b312d9dbd202a174b37ced90332f589a3cd4fbd
Marcio Lima <marcio.lima.inacio@gmail.com>
2017-05-30 19:00:08 -0300

    Modificação em desparentize
    Tratamento para lidar com estruturas que não são átomos nem variáveis
    nem listas.

--- trabalhos/t1.pl
+++ trabalhos/t1.pl
@@ -3,9 +3,12 @@ desparentize([Cabeca | Cauda], [Cabeca | CaudaOut]) :- atomic(Cabeca),
                                                      desparentize(Cauda, CaudaOut), !.
 desparentize([Cabeca | Cauda], ListaOut) :- var(Cabeca),
                                             desparentize(Cauda, ListaOut), !.
-desparentize([Cabeca | Cauda], ListaOut) :- desparentize(Cabeca, Aux1),
+desparentize([Cabeca | Cauda], ListaOut) :- is_list(Cabeca),
+                                            desparentize(Cabeca, Aux1),
                                             desparentize(Cauda, Aux2),
-                                            append(Aux1, Aux2, ListaOut).
+                                            append(Aux1, Aux2, ListaOut), !.
+desparentize([Cabeca | Cauda], ListaOut) :- desparentize(Cauda, ListaOut).
+
 
 tira_nao_comuns([], _, []) :- !.
 tira_nao_comuns([Cabeca | Cauda], Lista2, [Cabeca | CaudaOut]) :- member(Cabeca, Lista2),
