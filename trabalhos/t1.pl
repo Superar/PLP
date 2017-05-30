c711dd880cd25c4f07bce8df502c088b0ec3860f
Marcio Lima <marcio.lima.inacio@gmail.com>
2017-05-30 19:50:46 -0300

    Criação de predicados monta_pares e conta_atomos
    monta_pares cria uma lista com pares representando cada elemento da
    lista e quantas vezes ele aparece.
    
    conta_atomos implementa uma lista de pares representando cada elemento
    em comum entre duas listas e quantas vezes ele aparece em ambas.

--- trabalhos/t1.pl
+++ trabalhos/t1.pl
@@ -28,3 +28,17 @@ quantas_vezes(X, [_ | Cauda], N) :- quantas_vezes(X, Cauda, N).
 elimina_ocorrencias(_, [], []) :- !.
 elimina_ocorrencias(X, [X | Cauda], Lout) :- elimina_ocorrencias(X, Cauda, Lout), !.
 elimina_ocorrencias(X, [Cabeca | Cauda], [Cabeca | CaudaOut]) :- elimina_ocorrencias(X, Cauda, CaudaOut).
+
+
+monta_pares([], []) :- !.
+monta_pares([Cabeca | Cauda], [[Cabeca, N] | CaudaOut]) :- quantas_vezes(Cabeca, [Cabeca | Cauda], N),
+                                                           elimina_ocorrencias(Cabeca, Cauda, ListaAux),
+                                                           monta_pares(ListaAux, CaudaOut).
+
+
+conta_atomos(Lista1, Lista2, ListaOut) :- desparentize(Lista1, Aux1),
+                                          desparentize(Lista2, Aux2),
+                                          tira_nao_comuns(Aux1, Aux2, ListaFinal1),
+                                          tira_nao_comuns(Aux2, Aux1, ListaFinal2),
+                                          append(ListaFinal1, ListaFinal2, ListaFinal),
+                                          monta_pares(ListaFinal, ListaOut).
