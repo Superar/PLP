36a4514a0e0d040cf15f7a4106d1dbd947658a26
Marcio Lima <marcio.lima.inacio@gmail.com>
2017-05-30 18:39:41 -0300

    Adição do predicado tira_nao_comuns

--- trabalhos/t1.pl
+++ trabalhos/t1.pl
@@ -1,8 +1,13 @@
 desparentize([], []) :- !.
-desparentize([Cabeca | Cauda], [Cabeca | Cauda2]) :- atomic(Cabeca),
-                                                     desparentize(Cauda, Cauda2), !.
+desparentize([Cabeca | Cauda], [Cabeca | CaudaOut]) :- atomic(Cabeca),
+                                                     desparentize(Cauda, CaudaOut), !.
 desparentize([Cabeca | Cauda], ListaOut) :- var(Cabeca),
                                             desparentize(Cauda, ListaOut), !.
 desparentize([Cabeca | Cauda], ListaOut) :- desparentize(Cabeca, Aux1),
                                             desparentize(Cauda, Aux2),
                                             append(Aux1, Aux2, ListaOut).
+
+tira_nao_comuns([], _, []) :- !.
+tira_nao_comuns([Cabeca | Cauda], Lista2, [Cabeca | CaudaOut]) :- member(Cabeca, Lista2),
+                                                                  tira_nao_comuns(Cauda, Lista2, CaudaOut), !.
+tira_nao_comuns([_ | Cauda], Lista2, ListaOut) :- tira_nao_comuns(Cauda, Lista2, ListaOut).
