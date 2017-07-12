2fbd6be379ec60a0530c319d453f403241470a6e
Marcio Lima <marcio.lima.inacio@gmail.com>
2017-07-12 16:23:08 -0300

    Criação de novas Threads

--- trabalhos/t3/src/com/company/SistemaDeReservas.java
+++ trabalhos/t3/src/com/company/SistemaDeReservas.java
@@ -14,11 +14,27 @@ public class SistemaDeReservas
         ReservaThread thread1 = new ReservaThread("Nicanor", ats);
         System.out.println(thread1);
 
-        ReservaThread thread2 = new ReservaThread("SECOMP", ats);
+        ReservaThread thread2 = new ReservaThread("Vania", ats);
         System.out.println(thread2);
+
+        ReservaThread thread3 = new ReservaThread("Heloisa", ats);
+        System.out.println(thread3);
+
+        ReservaThread thread4 = new ReservaThread("Valter", ats);
+        System.out.println(thread1);
+
+        ReservaThread thread5 = new ReservaThread("Helena", ats);
+        System.out.println(thread2);
+
+        ReservaThread thread6 = new ReservaThread("Helio", ats);
+        System.out.println(thread3);
 
         // Executa as Threads
         new Thread(thread1).start();
         new Thread(thread2).start();
+        new Thread(thread3).start();
+        new Thread(thread4).start();
+        new Thread(thread5).start();
+        new Thread(thread6).start();
     }
 }
